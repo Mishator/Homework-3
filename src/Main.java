e53c7afe9a995b72fee4b7d66a869eff8c1f7937
Mishator <voyteh89@mail.ru>
2023-02-21 00:36:51 +0300

    Выполнена задача 5, инициализировал переменную frog.

--- src/Main.java
+++ src/Main.java
@@ -52,5 +52,15 @@ public static void task4 () {
             System.out.println(friend);
 }
 
-
+public static void task5 () {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+}
 }
