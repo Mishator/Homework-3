452f58293e8ca9dcf35b81babd085f785ecdd404
Mishator <voyteh89@mail.ru>
2023-02-20 23:09:34 +0300

    Выполнена задача 2, увеличел значения переменных на 4.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
 
     }
@@ -12,5 +13,18 @@ public static void task1 () {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+}
+public static void task2 () {
+        System.out.println("Задача 2");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
 }
 }
